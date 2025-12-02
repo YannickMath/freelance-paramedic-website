119840203967b1b7f372fe44d9710783ad272d08
Yannick MATH <yannick_math@yahoo.fr>
2025-12-02 23:50:18 +0100

    css review

--- components/NettoyageEnergetique.js
+++ components/NettoyageEnergetique.js
@@ -24,7 +24,7 @@ export default function NettoyageEnergetique() {
           style={{ filter: "brightness(0.7) saturate(1.1)" }}
         />
       </div>
-      <div className="relative z-10 min-h-screen flex flex-col justify-center py-8 px-4 sm:px-6 lg:px-8 pt-44 sm:pt-24">
+      <div className="relative z-10 min-h-screen flex flex-col justify-center py-8 px-4 sm:px-6 lg:px-8 mt-24 sm:mt-10">
         <div className="max-w-4xl mx-auto w-full">
           <div className="bg-white/50 backdrop-blur-sm rounded-3xl shadow-2xl p-8 sm:p-10 md:p-12 lg:p-16 space-y-6 lg:space-y-8 animate-scale-in">
             <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-primary-700 mb-4 text-center animate-fade-in">
